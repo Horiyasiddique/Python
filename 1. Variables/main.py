6cb90e169d6d908768b8a444c3d9f71a62554d3a
Horiyasiddique <horiyasiddique52@gmail.com>
2026-08-24 20:02:10 +0500

    Variables

---
+++ 1. Variables/main.py
@@ -0,0 +1,19 @@
+# (1) First program in Python
+print("Assalam O Alaikum")
+
+# (2) Variables 
+# You can not use numbers at variable start
+# You can not use spaces in variables.
+# You should not use special characters in variables.
+name = "Hooriya"
+age = 16
+matric = True
+
+print(name)
+print(age)
+print(matric)
+
+#Naming cnventions in Python
+fullName = "Hooriya Siddiqui" # camel case
+FullName = "Hooriya Siddiqui" # pascal case
+full_name = "Hooriya Siddiqui" #snake case
