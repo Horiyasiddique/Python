c4506a0324ab35fe094e86039336fdfe2db809da
Horiyasiddique <horiyasiddique52@gmail.com>
2026-08-25 19:38:59 +0500

    Data types

---
+++ 2. Data types/main.py
@@ -0,0 +1,40 @@
+# (3) Data types
+
+# i. Number
+# (a) Integer: - All the numbers excluding decimal places and fraction.
+int = 2
+int2 = -6
+print(type(int))
+print(type(int2))
+
+# (b) Float: - All the decimal numbers and fraction values are Float.
+flo = 2.6
+pie = 22/7
+frac = 12/3
+print(type(flo))
+print(type(pie))
+print(type(frac))
+
+# (c) Complex: - Numbers with real and imaginary parts are complex.
+cmplx = 8j
+print(type(cmplx))
+
+# -------------------------------------
+
+# ii. String : - anything written in "" or ''
+str = "abc"
+str2 = "123"
+str3 = "@#$"
+print(type(str))
+print(type(str2))
+print(type(str3))
+
+# ------------------------------------
+
+# iii. Boolean : -True or False
+is_matric = True
+is_graduated = False
+print(type(is_matric))
+print(type(is_graduated))
+
+# --------------------------------------
